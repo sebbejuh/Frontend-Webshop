14435e09dca6394d4bcdd68fd1015f4b9ac54e2d
Sebastian Locksenius <slocksenius@gmail.com>
2023-09-15 00:07:46 +0200

    some comments

--- src/context/ShopContext.tsx
+++ src/context/ShopContext.tsx
@@ -2,8 +2,9 @@ import { createContext, useState, useEffect, ReactNode  } from 'react';
 import { allProducts } from '../products';
 import { CartItemInterface, ShopContextValueInterface } from '../types/contextTypes';
 
-const ShopContext = createContext<ShopContextValueInterface | null>(null);//set to null since undefined is bad
+const ShopContext = createContext<ShopContextValueInterface | null>(null);// creates context with value null
 
+// GET DEFAULT CART FUNCTION
 const getDefaultCart = () => {
   const cart: CartItemInterface = {};
   for (let i = 1; i < allProducts.length + 1; i++) {
@@ -11,20 +12,20 @@ const getDefaultCart = () => {
   }
   return cart;
 };
-
+// CONTEXT PROVIDER FUNCTION
 const ShopContextProvider = (props: { children: ReactNode }) => {
 
   const [cartItems, setCartItems] = useState<CartItemInterface>(() => {//useState
-    
+    // Initialize the cart from localStorage, or use the default cart if no data is found
     const storedCartString = localStorage.getItem('cart');
     const storedCart = storedCartString ? JSON.parse(storedCartString) : getDefaultCart();
-    return storedCart || getDefaultCart();  // Initialize the cart from localStorage, or use the default cart if no data is found
+    return storedCart || getDefaultCart();
   });
 
   useEffect(() => {// Save cart data to localStorage whenever cartItems changes
     localStorage.setItem('cart', JSON.stringify(cartItems));
   }, [cartItems]);
-
+  // GET CART AMOUNT FUNCTION
   const getCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
@@ -37,17 +38,17 @@ const ShopContextProvider = (props: { children: ReactNode }) => {
     }
     return totalAmount;
   };
-
+  // ADD TO CART FUNCTION
   const addToCart = (itemId: number) => { //takes in an id of a product
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));//takes in previous state of cartItems and adds 1 to the itemId.
   };
-
+  // REMOVE FROM CART FUNCTION
   const removeFromCart = (itemId: number) => {
     if (cartItems[itemId] > 0) {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     }
   };
-
+  // UPDATE CART FUNCTION
   const updateCartItem = (newAmount: number, itemId: number) => {//takes in a new amount of products for an id
     setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));//replaces the previous amount with the new
   };
