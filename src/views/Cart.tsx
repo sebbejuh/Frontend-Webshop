ac85226496b4967ef6eac7f1ea268354877d1742
Sebastian Locksenius <slocksenius@gmail.com>
2023-09-30 23:24:41 +0200

    Made the Orders view functional

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -7,7 +7,7 @@ const Cart = () => {
   const products: (ProductInterface | ExtendedProductInterface)[] = productsJSON.products; //creates products variable to hold the products array from the JSON file
   const { cartItems, getCartAmount } = useShopContext();
   const totalAmount = getCartAmount()
-  
+
   const addToOrder = () => {
     const order = {
       cart: cartItems,
@@ -26,7 +26,7 @@ const Cart = () => {
     <section className="cart">
       <h1>Kundvagn</h1>
       <ul className="cart-list">
-        {products.map((product) => {//maps through allProducts array
+        {products.map((product) => {//maps through products array
           if (cartItems[product.id] !== 0) {//if product id isnt 0 (product ids starts at 1 and increases)
             return <CartItem key={product.id} data={product} />//passes product object through props
           }
--- src/views/Orders.tsx
+++ src/views/Orders.tsx
@@ -1,8 +1,57 @@
-
+import productsJSON from '../fake-db/products.json'
+import { useState } from 'react';
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
+  const products: (ProductInterface | ExtendedProductInterface)[] = productsJSON.products; //creates products variable to hold the products array from the JSON file
+  // get the orders from localStorage
+  const [orders, setOrders] = useState(
+    JSON.parse(localStorage.getItem('orders') || '[]')
+  );
+// Function to remove an order by index
+const removeOrder = (index) => {
+  const updatedOrders = [...orders];
+  updatedOrders.splice(index, 1);
+  setOrders(updatedOrders);
+  localStorage.setItem('orders', JSON.stringify(updatedOrders));
+};
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+  return date.toLocaleString('sv-SE', options);
+};
+
+return (
+  <section className="orders">
+    <h1>Ordrar</h1>
+    <ul className="order-list">
+      {orders.map((order, index) => (
+        <li key={index}>
+          <p>Datum: {formatDate(order.date)}</p>
+          <ul>
+            {Object.keys(order.cart).map((productId) => {
+              const product = products.find((p) => p.id === parseInt(productId));
+              if (product && order.cart[productId] !== 0) {
+                return (
+                  <li key={productId}>
+                    {order.cart[productId]} {product.name}
+                  </li>
+                );
+              }
+              return null;
+            })}
+          </ul>
+          <button onClick={() => removeOrder(index)}>Ta bort order</button>
+        </li>
+      ))}
+    </ul>
+  </section>
   )
 }
 
